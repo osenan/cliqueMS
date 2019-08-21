535ef4526729c70695dd1ae712cef20aeb09d583
osenan <oriol.senan@urv.cat>
2019-08-21 21:35:36 +0200

    Correction of NOTES, ERRORS and WARNINGS from R CMD BiocCheck
    Former-commit-id: ae33e81d441930aa8ded7dbc6e7d88a948ec78ea [formerly e17879c4b8cdfe253886a08419eb16e4415fdb17]
    Former-commit-id: f8c5ec511bf68ad5288c30e711660a7949d01e11

--- R/findCliques.R
+++ R/findCliques.R
@@ -5,32 +5,32 @@
 ##################################################
 
 updateCliques <- function(anclique, cliques) {
-    # this function is to assign a clique group value to nodes
-    # that do not have links, because they do not appear in
-    # the edgelist
-    # number of nodes without links, not in the clique data.frame
+    ## this function is to assign a clique group value to nodes
+    ## that do not have links, because they do not appear in
+    ## the edgelist
+    ## number of nodes without links, not in the clique data.frame
     cliques = cliques[order(cliques[,"node"]),]
-    # assign correct node value to "nodes"
+    ## assign correct node value to "nodes"
     cliques[,"node"] <- igraph::V(anclique$network)$id
     maxCliqueLabel <- max(cliques[,"clique"]) + 1
-    # search for nodes without clique value
-    missingNodes <- (1:nrow(anclique$peaklist))[
-        is.na(match((1:nrow(anclique$peaklist)),
-                    cliques[,"node"]))]
+    ## search for nodes without clique value
+    missingNodes <- seq_len(nrow(anclique$peaklist))[
+        is.na(match(seq_len(nrow(anclique$peaklist)),
+            cliques[,"node"]))]
     if( length(missingNodes) > 0 ) {
-        # if nodes without clique value exist
+        ## if nodes without clique value exist
         templateC <- rep(1, length(missingNodes))
         templateC[1] <- maxCliqueLabel
-        # assign clique values to this nodes
+        ## assign clique values to this nodes
         newCliques <- cumsum(templateC)
         dfnewCliques <- data.frame(node = missingNodes,
-                                   clique = newCliques)
+            clique = newCliques)
         dfnewCliques <- as.matrix(dfnewCliques)
         cliques = rbind(cliques, dfnewCliques)
-        # join this nodes and order again the matrix
+        ## join this nodes and order again the matrix
         cliques = cliques[order(cliques[,"node"]),]
     }
-    # clique label that is not in the returned ones
+    ## clique label that is not in the returned ones
     return(cliques[,"clique"])
 }
 
@@ -58,21 +58,22 @@ updateCliques <- function(anclique, cliques) {
 #' ppm = 15, peakwidth = c(5,20), snthresh = 10)
 #' ex.anClique <- createanClique(msSet)
 #' summary(ex.anClique)
-#' netlist <- createNetwork(msSet, msSet@peaks, filter = TRUE)
+#' netlist <- createNetwork(msSet, peaks(msSet), filter = TRUE)
 #' @seealso \code{\link{getCliques}}
 computeCliques <- function(anclique, tol = 1e-5, silent = TRUE) {
-    # this function calls the C++ code and the probabilistic model
-    # to find the clique groups with max log-likelihhod. Then it assigns clique groups to
-    # features that have links and then assigns clique groups to the remaining features
-    # It is recommended first to apply filters to anClique peaklist
+    ## this function calls the C++ code and the probabilistic model
+    ## to find the clique groups with max log-likelihhod.
+    ## Then it assigns clique groups to features that have links and
+    ## then assigns clique groups to the remaining features
+    ## It is recommended first to apply filters to anClique peaklist
     if(anclique$cliquesFound == TRUE) {
         warning("cliques have already been computed\n")
     }
     # create network data.frame
     netdf <- cbind(igraph::as_edgelist(anclique$network),
-                  igraph::E(anclique$network)$weight)
+        igraph::E(anclique$network)$weight)
     netdf <- data.frame(node1 = netdf[,1], node2 = netdf[,2],
-                       weight = netdf[,3])
+        weight = netdf[,3])
     cliquesRaw <- returnCliques(netdf, tol, silent = silent) # computeCliques
     cliquesGood <- updateCliques(anclique, cliquesRaw)
     anclique$peaklist$cliqueGroup = cliquesGood
@@ -126,24 +127,23 @@ computeCliques <- function(anclique, tol = 1e-5, silent = TRUE) {
 #' clique groups. It adds the column 'cliqueGroup' to the
 #' 'peaklist' in the 'anClique' object.
 #' @examples
-#' \donttest{
 #' mzfile <- system.file("standards.mzXML", package = "cliqueMS")
 #' msSet <- xcms::xcmsSet(files = mzfile, method = "centWave",
 #' ppm = 15, peakwidth = c(5,20), snthresh = 10)
 #' ex.cliqueGroups <- getCliques(msSet)
-#' }
 #' @seealso \code{\link{computeCliques}}
 #' \code{\link{createNetwork}}
 #' \code{\link{anClique}}
-getCliques <- function(mzData, filter = TRUE, mzerror = 5e-6, intdiff = 1e-4, rtdiff = 1e-4, tol = 1e-5, silent = TRUE) {
+getCliques <- function(mzData, filter = TRUE, mzerror = 5e-6,
+    intdiff = 1e-4, rtdiff = 1e-4, tol = 1e-5, silent = TRUE) {
     cat("Creating anClique object\n")
     anclique <- createanClique(mzData)
     cat("Creating network\n")
     netlist <- createNetwork(mzData, anclique$peaklist,
-                                      filter = filter,
-                                      mzerror = mzerror,
-                                      intdiff = intdiff,
-                                      rtdiff = rtdiff)
+        filter = filter,
+        mzerror = mzerror,
+        intdiff = intdiff,
+        rtdiff = rtdiff)
     anclique$peaklist <- netlist$peaklist
     anclique$network <- netlist$network
     cat("Computing cliques\n")
--- R/findIsotopes.R
+++ R/findIsotopes.R
@@ -1,47 +1,46 @@
 filterIso <- function(isodf, network) {
-    # Function to filter isotopes data.frame,
-    # and to create a network of isotopes
-    netpfeature = sapply(isodf$pfeature, function(x) {
+    ## Function to filter isotopes data.frame,
+    ## and to create a network of isotopes
+    netpfeature = vapply(isodf$pfeature, function(x) {
         which(igraph::V(network)$id == x)
-    })
-    netifeature = sapply(isodf$ifeature, function(x) {
+    }, numeric(1))
+    netifeature = vapply(isodf$ifeature, function(x) {
         which(igraph::V(network)$id == x)
-    })
+    }, numeric(1))
     isodf$pfeature = netpfeature
     isodf$ifeature = netifeature
     isodfSorted <- isodf[,c("pfeature", "ifeature")]
-    isodfSorted <- do.call(rbind,lapply(1:nrow(isodfSorted),
-                                        function(x) {
-                                            sort(isodfSorted[x,]) }
-                                        ))
+    isodfSorted <- do.call(rbind,lapply(seq_len(nrow(isodfSorted)),
+        function(x) { sort(isodfSorted[x,]) }
+    ))
     isodf$weight <- igraph::E(network,
-                              P = as.numeric(t(isodfSorted)))$weight
-    #as.numeric is important to acces correct weight values
-    #First filter isotopes pointing to two different parents
+        P = as.numeric(t(isodfSorted)))$weight
+    ## as.numeric is important to acces correct weight values
+    ## First filter isotopes pointing to two different parents
     inlinks <- as.numeric(names(
         which(table(isodf[,"ifeature"]) > 1)))
     badpfeatures <- unlist(lapply(inlinks, function(x) {
         rowpfeatures <- which(isodf$ifeature == x)
-        #drop the parental feature with less weight
+        ## drop the parental feature with less weight
         dropRows <- rowpfeatures[-1*which.max(
             isodf[rowpfeatures,"weight"])]
     }))
     if( length(badpfeatures) > 0 ) {
         isodf <- isodf[-1*badpfeatures,]
     }
-    #Second filter parents pointed by two diferent isotopes
+    ## Second filter parents pointed by two diferent isotopes
     outlinks <- as.numeric(names(
         which(table(isodf[,"pfeature"]) > 1)))
     badifeatures <- unlist(lapply(outlinks, function(x) {
         rowifeatures <- which(isodf$pfeature == x)
-        #drop the parental feature with less weight
+        ## drop the parental feature with less weight
         dropRows <- rowifeatures[-1*which.max(
         isodf[rowifeatures,"weight"])]
     }))
     if( length(badifeatures) > 0 ) {
         isodf <- isodf[-1*badifeatures,]
     }
-    #Third filter inconsistency in charge
+    ## Third filter inconsistency in charge
     allnodes <- unique(c(isodf[,"pfeature",],isodf[,"ifeature"]))
     badisoCharge <- unlist(lapply(allnodes, function(x) {
         res <- integer()
@@ -59,20 +58,21 @@ filterIso <- function(isodf, network) {
     if( length(badisoCharge) > 0 ) {
         isodf <- isodf[-1*badisoCharge,]
     }
-    realpfeature <- sapply(isodf$pfeature, function(x) {
-        igraph::V(network)[x]$id })
-    realifeature <- sapply(isodf$ifeature, function(x) {
-        igraph::V(network)[x]$id })
+    realpfeature <- vapply(isodf$pfeature, function(x) {
+        igraph::V(network)[x]$id }, numeric(1))
+    realifeature <- vapply(isodf$ifeature, function(x) {
+        igraph::V(network)[x]$id }, numeric(1))
     isodf$pfeature <- realpfeature
     isodf$ifeature <- realifeature
-    # Finally create the filtered isotope network
+    ## Finally create the filtered isotope network
     isonet <- igraph::graph.data.frame(isodf[,c("ifeature","pfeature")])
     return(list(network = isonet, isodf = isodf))
 }
 
 isoGrade <- function(isonet) {
-    # Function to grade and isotope, starting from 0 to the parental isotpe, 1 the first isotope and further
-    grades <- sapply(igraph::V(isonet), function(x) {
+    ## Function to grade and isotope, starting from 0 to the parental isotpe,
+    ## 1 the first isotope and further
+    grades <- vapply(igraph::V(isonet), function(x) {
         res <- 0
         nei <- igraph::neighbors(isonet, v = x, mode = "out")
         while(length(nei) > 0) {
@@ -80,14 +80,14 @@ isoGrade <- function(isonet) {
             nei <- igraph::neighbors(isonet, v = nei, mode = "out")
         }
         res
-    })
+    }, numeric(1))
     return(grades)
 }
 
 correctGrade <- function(isoTable, maxGrade) {
     maxCluster <- max(isoTable$cluster)
     clusters <- unique(isoTable$cluster)
-    res <- do.call(rbind,lapply(1:length(clusters), function(x) {
+    res <- do.call(rbind,lapply(seq_len(length(clusters)), function(x) {
         cluster <- clusters[x]
         cpos <- isoTable[isoTable$cluster == x,]
         goodP <- cpos[cpos$grade <= maxGrade,]
@@ -105,11 +105,11 @@ correctGrade <- function(isoTable, maxGrade) {
             
 
 isonetAttributes <- function(isolist, maxGrade) {
-    # Function to set the node attributes for each isotope:
-    # grade, charge, and community
-    # First assign grade (for info look isoGrade function)
+    ## Function to set the node attributes for each isotope:
+    ## grade, charge, and community
+    ## First assign grade (for info look isoGrade function)
     igraph::V(isolist$network)$grade <- isoGrade(isolist$network)
-    charge <- sapply(
+    charge <- vapply(
         as.numeric(igraph::V(isolist$network)$name), function(x) {
             posp <- which(isolist$isodf[,"pfeature"] == x)
             if( length(posp) > 0 ) {
@@ -119,21 +119,20 @@ isonetAttributes <- function(isolist, maxGrade) {
                 res <- isolist$isodf[posi,"icharge"]
             }
             res
-        })
-    # Second assign charge of each feature as isotope
+        }, numeric(1))
+    ## Second assign charge of each feature as isotope
     igraph::V(isolist$network)$charge <- charge
-    # Third label features that belong to the same isotope cluster
+    ## Third label features that belong to the same isotope cluster
     igraph::V(isolist$network)$cluster <- 
-                                 igraph::clusters(isolist$network,
-                                                  "weak")$membership
-    # Final step write a table with all the isotope data
+        igraph::clusters(isolist$network, "weak")$membership
+    ## Final step write a table with all the isotope data
     isoTable <- data.frame(
         feature = as.numeric(igraph::V(isolist$network)$name),
         charge = igraph::V(isolist$network)$charge,
         grade = igraph::V(isolist$network)$grade,
         cluster = igraph::V(isolist$network)$cluster
     )
-    # Fourth, correct the grade
+    ## Fourth, correct the grade
     while(max(isoTable$grade) > maxGrade) {
         isoTable <- correctGrade(isoTable, maxGrade)
     }
@@ -144,8 +143,8 @@ addIso2peaklist <- function(isoTable, peaklist) {
     peaklist$isotope <- rep("M0", nrow(peaklist))
     peaklist$isotope[isoTable[,"feature"]] <- 
         paste(paste("M",isoTable$grade, sep = ""),
-              paste("[",isoTable$cluster, "]",sep = ""),
-              sep = " ")
+            paste("[",isoTable$cluster, "]",sep = ""),
+                sep = " ")
     return(peaklist)
 }
 
@@ -175,8 +174,7 @@ addIso2peaklist <- function(isoTable, peaklist) {
 #' @seealso
 #' \code{\link{getCliques}}
 getIsotopes <- function(anclique, maxCharge = 3,
-                                 maxGrade = 2, ppm = 10,
-                                 isom = 1.003355) {
+    maxGrade = 2, ppm = 10, isom = 1.003355) {
     # Function to get all the isotopes from the m/z data
     # after splitting it into clique groups
     if(anclique$isoFound == TRUE) {
@@ -184,28 +182,27 @@ getIsotopes <- function(anclique, maxCharge = 3,
     }
     if(anclique$cliquesFound == FALSE) {
         warning(paste("Cliques have not been computed for this object\n",
-                      "This could lead to long computing times",
-                      "for isotope annotation\n"))
+            "This could lead to long computing times",
+            "for isotope annotation\n"))
     }
     cat("Computing isotopes\n")
     listofisoTable <- lapply(anclique$cliques, function(x) {
-         df.clique <- as.data.frame(
+        df.clique <- as.data.frame(
             cbind(anclique$peaklist[x, c("mz","maxo")],x)
         )
         colnames(df.clique) <- c("mz","maxo","feature")
-        # Sort df.clique by intensity because isotopes are less
-        # intense than their parental features
+        ## Sort df.clique by intensity because isotopes are less
+        ## intense than their parental features
         df.clique <- df.clique[order(df.clique$maxo, decreasing = TRUE),]
-        # compute isotopes from clique
+        ## compute isotopes from clique
         isodf <- returnIsotopes(df.clique, maxCharge = maxCharge,
-                               ppm = ppm,
-                               isom = isom)
+            ppm = ppm, isom = isom)
         if( nrow(isodf) > 0 ) {
-            # filter the isotope list by charge
-            # and other inconsistencies
+            ## filter the isotope list by charge
+            ## and other inconsistencies
             isolist <- filterIso(isodf, anclique$network)
             if( nrow(isolist$isodf) > 0 ) {
-            # write a table with feature, charge, grade and cluster 
+            ## write a table with feature, charge, grade and cluster 
                 iTable <- isonetAttributes(isolist, maxGrade)
             } else {
                 iTable = NULL}
@@ -214,32 +211,31 @@ getIsotopes <- function(anclique, maxCharge = 3,
         }
         iTable
     })
-    # If there are no isotopes in all dataset
+    ## If there are no isotopes in all dataset
     if( length(listofisoTable) ==
-       sum(sapply(listofisoTable, is.null)) ) {
-           isoTable <- matrix(c(NA,NA,NA,NA), nrow = 1)
-           colnames(isoTable) <- c("feature","charge","grade","cluster")
-           anclique$peaklist$isotope <- rep("M0",
-                                            nrow(anclique$peaklist))
-           } else {
-    # The cluster label is inconsistent between all isotopes found
-    # let's correct for avoiding confusions
-               listofisoTable <- listofisoTable[!sapply(listofisoTable, is.null)]
-               maxC <- max(listofisoTable[[1]]$cluster)
-               for(i in 2:length(listofisoTable)) {
-                   listofisoTable[[i]]$cluster = listofisoTable[[i]]$cluster + maxC + 1
-                   maxC <- max(listofisoTable[[i]]$cluster)
-               }
-               isoTable <- do.call(rbind, listofisoTable)
-               rownames(isoTable) <- 1:nrow(isoTable)
-               # Change the peaklist adding isotope column
-               anclique$peaklist <- addIso2peaklist(isoTable,
-                                                    anclique$peaklist)
-           }
+        sum(vapply(listofisoTable, is.null, logical(1))) ) {
+            isoTable <- matrix(c(NA,NA,NA,NA), nrow = 1)
+            colnames(isoTable) <- c("feature","charge","grade","cluster")
+            anclique$peaklist$isotope <- rep("M0", nrow(anclique$peaklist))
+    } else {
+    ## The cluster label is inconsistent between all isotopes found
+    ## let's correct for avoiding confusions
+        listofisoTable <- listofisoTable[
+            !vapply(listofisoTable, is.null, logical(1))]
+        maxC <- max(listofisoTable[[1]]$cluster)
+        for(i in 2:length(listofisoTable)) {
+            listofisoTable[[i]]$cluster = listofisoTable[[i]]$cluster + maxC + 1
+            maxC <- max(listofisoTable[[i]]$cluster)
+            }
+            isoTable <- do.call(rbind, listofisoTable)
+            rownames(isoTable) <- seq_len(nrow(isoTable))
+            ## Change the peaklist adding isotope column
+            anclique$peaklist <- addIso2peaklist(isoTable, anclique$peaklist)
+    }
     cat("Updating anClique object\n")
-    # Now change status of isotopes at anclique object
+    ## Now change status of isotopes at anclique object
     anclique$isoFound <- TRUE
-    # Put new isotopes table
+    ## Put new isotopes table
     anclique$isotopes <- isoTable
     return(anclique)
 }
