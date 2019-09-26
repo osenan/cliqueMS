3182e626139a78198be52cf081f1375398fec4f1
osenan <oriol.senan@urv.cat>
2019-09-26 14:58:03 +0200

    complete substitution of S3 for S4 anClique classes
    Former-commit-id: c4fcf07e465e0d9bb0eb62624b6c162bd7aa881a

--- tests/testthat/test_isotopes.R
+++ tests/testthat/test_isotopes.R
@@ -4,13 +4,15 @@ data(ex.cliqueGroups)
 isotopes <- getIsotopes(ex.cliqueGroups)
 
 test_that("Filter isotopes is working ", {
-    expect_equal(length(unique(isotopes$isotopes[,1])),
-                 length(isotopes$isotopes[,1]))
+    expect_equal(length(unique(getIsolistanClique(isotopes)[,1])),
+                 length(getIsolistanClique(isotopes)[,1]))
 })
 
 test_that("This features are correct isotopes", {
-    iso0 = isotopes$isotopes[isotopes$isotopes$feature == 122,]
-    iso1 = isotopes$isotopes[isotopes$isotopes$feature == 115,]
+    iso0 = getIsolistanClique(isotopes)[
+        getIsolistanClique(isotopes)$feature == 122,]
+    iso1 = getIsolistanClique(isotopes)[
+        getIsolistanClique(isotopes)$feature == 115,]
     expect_identical(iso0$cluster, iso1$cluster)
     expect_equal(iso0$grade, 0)
     expect_equal(iso1$grade, 1)
